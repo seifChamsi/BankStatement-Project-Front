5d7ec3f14934bb5e894de8272510b6bf6127a31e
Saifeddin Chamsi <Saifeddin.Chamsi@sousse.r-iset.tn>
2020-04-11 23:45:35 +0200

    fix heroku issues

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static("./dist/taysir-statement"));
 
 app.all("*", (req, res) => {
-  res.status(200).sendFile(__dirname + "/dist/index.html");
+  res.status(200).sendFile(__dirname + "/dist/taysir-statement/index.html");
 });
 
 app.listen(8080);
