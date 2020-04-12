c22982fc332d7c96bab1d5e9f382c2f7a004ecbf
Saifeddin Chamsi <Saifeddin.Chamsi@sousse.r-iset.tn>
2020-04-12 12:00:57 +0200

    fix the index

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("./dist/taysir-statement"));
+app.use(express.static("./dist/Statements-Project"));
 
 app.all("/*", (req, res) => {
-  res.status(200).sendFile("index.html", { root: "dist/taysir-statement" });
+  res.status(200).sendFile("index.html", { root: "dist/Statements-Project" });
 });
 
 app.listen(process.env.PORT || 8080, () => {
